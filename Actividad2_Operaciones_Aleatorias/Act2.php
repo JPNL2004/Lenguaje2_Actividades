b9bfeae6727e7a6306a4068b2cad80232d6a4bf8
JPNL2004 <noguerajesus088@gmail.com>
2025-10-24 18:17:57 -0400

    Add files via upload

---
+++ Actividad2_Operaciones_Aleatorias/Act2.php
@@ -0,0 +1,15 @@
+<?php
+
+$numero1 = rand(1, 100);
+$numero2 = rand(1, 100);
+$numero3 = rand(1, 100);
+
+
+$suma = $numero1 + $numero2 + $numero3;
+
+
+echo "Número 1: $numero1<br>";
+echo "Número 2: $numero2<br>";
+echo "Número 3: $numero3<br>";
+echo "La suma de los tres números es: $suma";
+?>
